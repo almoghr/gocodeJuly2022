e118c9c34e57f6ffc77a1f78a8f67c958d085965
almoghr <almoghr36@gmail.com>
2022-08-01 21:16:32 +0300

    2

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -48,8 +48,6 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // console.log(numbers)
 // console.log(lastNumInTheArr)
 
-
-
 // // console.log('length', numbers.length)
 // // const newLength = numbers.unshift(0)
 // // console.log('length', newLength)
@@ -75,11 +73,9 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //   return number*number > 25
 // })
 
-
 // console.log(highNumbers)
 
-
-var fruits = ['Apple','banana','grapes','mango','orange','grapes'];
+var fruits = ["Apple", "banana", "grapes", "mango", "orange", "grapes"];
 
 // function filterItems(query) {
 
@@ -100,29 +96,119 @@ var fruits = ['Apple','banana','grapes','mango','orange','grapes'];
 // const foundElInd = fruits.findIndex((fruit) => {return fruit === 'grapes'})
 // console.log(foundElInd)
 
-const joinedstr = fruits.join(' ')
-console.log(joinedstr)
-console.log(typeof joinedstr)
+// const joinedstr = fruits.join(' ')
+// console.log(joinedstr)
+// console.log(typeof joinedstr)
+
+// const googleSearchString = 'https://www.google.com/search?q=shalom&sxsrf=ALiCzsZzIYB6geGqWtFdc-BENLRDedqZqg%3A1659287728658&source=hp&ei=sLjmYtuWJOTk7_UPqoyAuAM&iflsig=AJiK0e8AAAAAYubGwP7fEc2e19Ii7AfiTnWiT1EO4m1n&ved=0ahUKEwjbrpK90KP5AhVk8rsIHSoGADcQ4dUDCAg&uact=5&oq=shalom&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEEMyBAguEEMyBAgAEEMyBwguENQCEEMyBAgAEEMyBAguEEMyBQgAEIAEMgUILhCABDIFCC4QgAQyCAguEIAEENQCOgcIIxDqAhAnOgQIIxAnOhEILhCABBCxAxCDARDHARDRAzoLCAAQgAQQsQMQgwE6DgguELEDEIMBEMcBENEDOggIABCxAxCDAToICC4QgAQQsQM6EAguELEDEIMBEMcBENEDEEM6CwguEIAEELEDEIMBOg0ILhCxAxCDARDUAhBDOgoIABCxAxCDARBDUOEBWK4GYJQIaAFwAHgAgAGbAYgB2gaSAQMwLjaYAQCgAQGwAQo&sclient=gws-wiz'
+
+// const splittedString = googleSearchString.split('/')
+
+// console.log(splittedString)
+// const lastElInSplittedString = splittedString[splittedString.length-1]
+
+// console.log(lastElInSplittedString)
+
+// const splittedQueryString = lastElInSplittedString.split('&')
+// console.log(splittedQueryString)
+
+// const newNumbers = numbers.map((number) => {
+//   return number**4
+// })
+
+// console.log(newNumbers)
+// console.log(numbers)
+
+// const array1 = [1, 2, 3, 4];
+// const reducer = (totalSum, currentCellInArray) => {
+//   return totalSum * currentCellInArray;
+// };
+
+// // 1 + 2 + 3 + 4
+// console.log(array1.reduce(reducer));
+// // expected output: 10
+
+// // 5 + 1 + 2 + 3 + 4
+// console.log(array1.reduce(reducer, 5));
+// // expected output: 15
+
+// function multiply(arr, n) {
+//   if (n <= 0) {
+//     return 1;
+//   } else {
+//     return multiply(arr, n - 1) * arr[n - 1];
+//   }
+// }
 
 
-const googleSearchString = 'https://www.google.com/search?q=shalom&sxsrf=ALiCzsZzIYB6geGqWtFdc-BENLRDedqZqg%3A1659287728658&source=hp&ei=sLjmYtuWJOTk7_UPqoyAuAM&iflsig=AJiK0e8AAAAAYubGwP7fEc2e19Ii7AfiTnWiT1EO4m1n&ved=0ahUKEwjbrpK90KP5AhVk8rsIHSoGADcQ4dUDCAg&uact=5&oq=shalom&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEEMyBAguEEMyBAgAEEMyBwguENQCEEMyBAgAEEMyBAguEEMyBQgAEIAEMgUILhCABDIFCC4QgAQyCAguEIAEENQCOgcIIxDqAhAnOgQIIxAnOhEILhCABBCxAxCDARDHARDRAzoLCAAQgAQQsQMQgwE6DgguELEDEIMBEMcBENEDOggIABCxAxCDAToICC4QgAQQsQM6EAguELEDEIMBEMcBENEDEEM6CwguEIAEELEDEIMBOg0ILhCxAxCDARDUAhBDOgoIABCxAxCDARBDUOEBWK4GYJQIaAFwAHgAgAGbAYgB2gaSAQMwLjaYAQCgAQGwAQo&sclient=gws-wiz'
+// var items = [
+//   { name: 'Edward', value: 21 },
+//   { name: 'And', value: 45 },
+//   { name: 'Magnetic', value: 13 },
+//   { name: 'Sharpe', value: 37 },
+//   { name: 'The', value: -12 },
+//   { name: 'Zeros', value: 37 }
+// ];
 
-const splittedString = googleSearchString.split('/')
+// // sort by value
+// const newItems = items.sort(function (a, b) {
+//   return b.value - a.value;
+// });
 
-console.log(splittedString)
-const lastElInSplittedString = splittedString[splittedString.length-1]
+// console.log(newItems)
 
-console.log(lastElInSplittedString)
+// // sort by name
 
-const splittedQueryString = lastElInSplittedString.split('&')
-console.log(splittedQueryString)
+// //פונקציית מיון לפי סטרינגים באנגלית.
+
+// const newItems2 = items.sort(function(a, b) {
+//   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+//   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+//   if (nameA < nameB) {
+//     return -1;
+//   }
+//   if (nameA > nameB) {
+//     return 1;
+//   }
+
+//   // names must be equal
+//   return 0;
+// });
+
+// console.log(newItems2)
 
 
-const newNumbers = numbers.map((number) => {
-  return number**4
-})
+// const numers = [1,6,3,8,3,4,0,11,32]
+// const wors = ['bat', 'dog', 'apple', 'change']
+// const result = numers.sort()
+// console.log(numers.sort())
+// console.log(wors.sort())
 
-console.log(newNumbers)
-console.log(numbers)
+
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min); 
+//   max = Math.floor(max); 
+//   const rand = Math.random()
+//   const end = Math.floor(rand * (max - min + 1)) + min;
+//   debugger
+//   return end
+// }
+
+// getRandomInt(4, 22); // 16 (For example)
 
 
+
+//for integers not arrays!!!
+
+// const randomNumber = Math.ceil(Math.random() * 6);
+// const randomNumber2 = Math.ceil(Math.random() * 6);
+// console.log(randomNumber, randomNumber2);
+
+const reverseStr = (string) => {
+  return string.split('').reverse().join('')
+  
+}
+let myStr = 'shalom lahem haverim'
+
+const newStr = reverseStr(myStr)
+console.log(newStr)
