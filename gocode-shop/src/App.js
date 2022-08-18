ab31edb6d7f4c8c2576f77d995ca34a2aee4258f
almoghr <almoghr36@gmail.com>
2022-08-18 20:45:27 +0300

    hooks and stuff :)

--- gocode-shop/src/App.js
+++ gocode-shop/src/App.js
@@ -1,65 +1,84 @@
-import logo from "./logo.svg";
-import "./App.css";
-import TimeOfDay from "./components/TimeOfDay";
-import Button from "./components/Button";
-import ListOfButtons from "./components/ListOfButtons";
+import React, {useState, useEffect, useRef} from 'react'
+import Button from './components/Button'
 
-function App() {
+const App = () => {
+    const refContainer = useRef(null);
 
+    const [count, setCount] = useState(0)    
+    const [showButton, setShowButton] = useState(true)
+    const [storeData, setStoreData] = useState([])
+    const [show,setShow] = useState(true)
+    const [fullName, setFullName] = useState({firstName: '', lastName:''})
+    const [val, setVal] = useState('')
+    const getApiAnswer = async () => {
+        try {
+          const response = await fetch('https://fakestoreapi.com/products')
+          const dataFromApi = await response.json()
+          setStoreData(dataFromApi)
+        } catch(e){
+            console.log(e)
+        }
+    }
+    
+    useEffect(() => {
+        console.log('moshe ogalbo')
+        // setCount(prev => prev + 1) // לולאה אינסופית!!!!!!
+    }, [count, showButton, storeData])
+    useEffect(() => {
+        console.log(fullName)
+    }, [fullName])
+
+    useEffect(() => {
+        console.log(storeData)
+        localStorage.setItem('storeData', JSON.stringify(storeData))
+
+    }, [storeData])
+
+
+    useEffect(() => {
+        getApiAnswer()
+        
+        // return () => {} // לשימוש רק כאשר רוצים לסגור את הקומפוננטה ורוצים שיקרה דבר מה לפני כן.
+    },[])
+
+
+
+    const handleCounterMinus = () => {
+        if(count === 0 ){
+            return
+        } else{
+            setCount((prev) => prev - 1)
+        }
+    }
+    
+        const handleCounterPlus = () => {
+            setCount((prev) => prev + 1)
+
+        }
+        
+        const toggleShowButton = () => {
+            setShowButton((prev) => !prev)
+        }    
+        useEffect(() => {
+            console.log('happened')
+            return () => {console.log('collapsed')}
+        },[show])
   return (
-    <>
-      {/* {isUserAdmin && <button>delete everything</button>} */}
-      <TimeOfDay />
-      <TimeOfDay />
-      <Button
-        text={"whatever"}
-        onClickHandler={() => {
-          console.log("the user has clicked");
-        }}
-        stylingObject={{ background: "red" }}
-      />
-      <Button
-        text={"something else"}
-        onClickHandler={() => {
-          console.log("wow");
-        }}
-        stylingObject={{ background: "blue" }}
-      />
-      <Button
-        text={"shalom lekulam"}
-        onClickHandler={() => {
-          alert("wow");
-        }}
-        stylingObject={{ background: "green" }}
-      />
-      <Button
-        text={"something else"}
-        onClickHandler={() => {
-          prompt("enter whatever you like");
-        }}
-        stylingObject={{ background: "gold" }}
-      />
-      <Button
-        text={"something dk;alsiuk"}
-        onClickHandler={() => {
-          prompt("enter whatever you like");
-        }}
-      />
-      <Button />
-      
-      {/* {buttonList} */}
+    <div>
+        {/* <input value={val} ref={refContainer} placeholder='mendy hatotah'onChange={(e) => setVal(e.target.value)}/> */}
+        <input value={fullName.firstName} placeholder='firstName' onChange={(e) => setFullName({firstName: e.target.value, lastName:fullName.lastName})}/>
+        <input value={fullName.lastName} placeholder='lastName' onChange={(e) => setFullName({firstName:fullName.firstName, lastName: e.target.value})}/>
 
-      {/* {listOfButtons.map((element, index) => (
-        <Button
-          key={index}
-          text={element.text}
-          onClickHandler={element.onClickHandler}
-          stylingObject={element.stylingObject}
-        />
-      ))} */}
-      <ListOfButtons />
-    </>
-  );
+        <Button text={"+"} onClickHandler={handleCounterPlus} />
+        <span>{count}</span>
+        <Button text={"-"} onClickHandler={handleCounterMinus} />
+        <Button text={showButton ? "hide me!!!" : "reveal me!!!!"} onClickHandler={toggleShowButton}/>
+        {showButton && <span>רואה ואינו נראה</span>}
+        {storeData ? storeData.map((element,index) => <div key={index}><span>{element.title}</span></div>) : 'no storeData found'}
+        {show && <Button text={"show"} onClickHandler={() => setShow(!show)}/> }
+        <button onClick={()=>{console.log(refContainer); refContainer.current.focus(); refContainer.current.value='almog'; refContainer.current.style.background = 'red';}}>click me pleaseee</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
