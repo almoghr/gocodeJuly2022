4c40045b30601ca57ca657074173531636094283
almoghr <almoghr36@gmail.com>
2022-08-29 18:47:33 +0300

    d

--- node1stLesson/index.js
+++ node1stLesson/index.js
@@ -1,21 +1,37 @@
-console.log(process.argv)
+const http = require("http");
+// console.log(process.argv)
 
-const makeASummary = () => {
-    let sum = 1
-    for(let i = 3; i < process.argv.length; i++){
-        if(process.argv[2] === 'plus'){
-            sum += parseInt(process.argv[i])
-        }
-        else if(process.argv[2] === 'minus'){
-            sum -= parseInt(process.argv[i])
-        }
-        else if(process.argv[2] === 'kefel'){
-            sum *= parseInt(process.argv[i])
-        }
-    }
-    return sum
-}
+// const makeASummary = () => {
+//     let sum = 1
+//     for(let i = 3; i < process.argv.length; i++){
+//         if(process.argv[2] === 'plus'){
+//             sum += parseInt(process.argv[i])
+//         }
+//         else if(process.argv[2] === 'minus'){
+//             sum -= parseInt(process.argv[i])
+//         }
+//         else if(process.argv[2] === 'kefel'){
+//             sum *= parseInt(process.argv[i])
+//         }
+//     }
+//     return sum
+// }
 
-const sum =  makeASummary()
+// const sum =  makeASummary()
 
-console.log(sum)
+// console.log(sum)
+
+// Load HTTP module
+
+// Create HTTP server and listen on port 8000 for requests
+http.createServer((request, response) => {
+
+   // Set the response HTTP header with HTTP status and Content type
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // Send the response body "Hello World"
+   response.end('Hello World\n');
+}).listen(8000);
+
+// Print URL for accessing server
+console.log('Server running at http://127.0.0.1:8000/');
