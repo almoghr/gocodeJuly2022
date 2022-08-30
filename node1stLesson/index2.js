10b4ad39ab4d356dc5d23a66131b1118d035af0d
almoghr <almoghr36@gmail.com>
2022-08-30 20:38:10 +0300

    Added all routes relevant - next stop - POSTMANgit add .git add .

--- node1stLesson/index2.js
+++ node1stLesson/index2.js
@@ -1,29 +1,32 @@
-const fs = require('fs')
-const products = require('./products')
+// const fs = require('fs')
+// const products = require('./products')
 
-const newProduct = {
-    "id": 22,
-    "title": "Laptops",
-    "price": 2394.95,
-    "description": "Stash padded sleeve, your everyday",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "rating": { "rate": 5, "count": 747 }
-  }
-  products.push(newProduct);
+// const newProduct = {
+//     "id": 22,
+//     "title": "Laptops",
+//     "price": 2394.95,
+//     "description": "Stash padded sleeve, your everyday",
+//     "category": "men's clothing",
+//     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+//     "rating": { "rate": 5, "count": 747 }
+//   }
+//   products.push(newProduct);
 
-  fs.writeFile("./products.json", JSON.stringify(products, (err) => { 
- 
-	// Checking for errors 
-	if (err) throw err; 
- 
-	console.log("Done writing"); // Success 
-})
-
-// fs.writeFile("./check.js", 'console.log(js)', (err) => { 
+//   fs.writeFile("./products.json", JSON.stringify(products, (err) => { 
  
 // 	// Checking for errors 
 // 	if (err) throw err; 
  
 // 	console.log("Done writing"); // Success 
-// }); // apperantly possible
+// })
+
+// // fs.writeFile("./check.js", 'console.log(js)', (err) => { 
+ 
+// // 	// Checking for errors 
+// // 	if (err) throw err; 
+ 
+// // 	console.log("Done writing"); // Success 
+// // }); // apperantly possible
+
+
+console.log(__dirname)
